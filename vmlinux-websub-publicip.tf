6a1c3e80c9954af6fbd7f2e59983cd5fd5a2788c
Gokulraj <mgokulraj.uk@gmail.com>
2023-12-20 17:42:21 +0000

    bastion-wrap

--- vmlinux-websub-publicip.tf
+++ vmlinux-websub-publicip.tf
@@ -1,8 +1,8 @@
-resource "azurerm_public_ip" "linuxvm_websub_publicip" {
-  name                = "${local.resource_name_prefix}-linuxvm-web-publicip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  domain_name_label   = "webserver-linux-ictay"
-}
+# resource "azurerm_public_ip" "linuxvm_websub_publicip" {
+#   name                = "${local.resource_name_prefix}-linuxvm-web-publicip"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+#   domain_name_label   = "webserver-linux-ictay"
+# }
