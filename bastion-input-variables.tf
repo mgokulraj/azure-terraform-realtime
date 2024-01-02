459bdb011d3c3d2cfd415658a4418df1e2cecbf4
Gokulraj <mgokulraj.uk@gmail.com>
2024-01-02 16:26:31 +0000

    vmss

--- bastion-input-variables.tf
+++ bastion-input-variables.tf
@@ -1,9 +1,9 @@
-# variable "bastion_service_subnet_name" {
-#   description = "Bastion Service Subnet Name"
-#   default     = "AzureBastionSubnet"
-# }
+variable "bastion_service_subnet_name" {
+  description = "Bastion Service Subnet Name"
+  default     = "AzureBastionSubnet"
+}
 
-# variable "bastion_service_subnet_address" {
-#   description = "Bastion Service Subnet Address"
-#   default     = ["10.0.101.0/27"]
-# }
+variable "bastion_service_subnet_address" {
+  description = "Bastion Service Subnet Address"
+  default     = ["10.0.101.0/27"]
+}
