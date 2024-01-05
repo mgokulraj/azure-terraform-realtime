78ec0a2d64154d4ca25cf884bd36940a185a1bf5
Gokulraj <mgokulraj.uk@gmail.com>
2024-01-05 15:08:23 +0000

    storage-account

--- storage-account.tf
+++ storage-account.tf
@@ -17,3 +17,16 @@ resource "azurerm_storage_container" "httpd_files_container" {
   storage_account_name = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
+
+locals {
+  httpd_conf_files = ["app1.conf"]
+}
+
+resource "azurerm_storage_blob" "httpd_files_container_blob" {
+  for_each = toset(local.httpd_conf_files)
+  name                   = each.value
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.httpd_files_container.name
+  type                   = "Block"
+  source = "${path.module}/app-scripts/${each.value}"
+}
